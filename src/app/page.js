a845c8a3df1ce6162aa78c82a211ab2bb9924031
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-09-10 18:53:40 -0600

    feat(add): Main

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,24 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      {/*<main className={styles.main}>
-        
-       
-      </main>*/}
+      <main className={styles.main}>
+            <div className={styles.ContainerImagen}>&nbsp;
+            <Image
+              src="/pics/4.jpg"
+              alt="imagen"
+              layout="responsive"
+              width={500}
+              height={50}
+              className={styles.animatedImage}
+              ></Image>
+            </div>
+            <div className={styles.ContainerTitle}>
+              <h1 className={styles.title}>Port</h1>
+              <h1 className={styles.title}>folio.</h1> 
+            </div>
+            
+
+      </main>
       
     </div>
   );
